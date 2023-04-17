6b62f3c96c70d96d20c2f55ee91e1b95b5f4e2ad
Michele <micheleddd@gmail.com>
2023-04-17 17:10:44 +0200

    correction 1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,6 @@ TAG=$1
 AWS_ECR_ACCOUNT_URL=$2
 AWS_ECR_HELM_REPO_NAME=$3
 echo "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME"
-eksctl create iamidentitymapping --cluster $AWS_EKS_CLUSTER_NAME --region $AWS_CLUSTER_REGION --arn $EKS_ARN --group system:masters --no-duplicate-arns --username $EKS_USER
 export KUBECONFIG=/home/circleci/.kube/config
 result=$(eval helm ls | grep flask-helm)
 if [ $? -ne "0" ]; then
