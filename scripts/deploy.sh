5360fd304710cfabbe9736400e56fcac0c03bb7d
Michele <micheleddd@gmail.com>
2023-04-17 16:28:38 +0200

    correction 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ TAG=$1
 AWS_ECR_ACCOUNT_URL=$2
 AWS_ECR_HELM_REPO_NAME=$3
 echo "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME"
-export KUBECONFIG=/home/circleci/.kube/config
+
 result=$(eval helm ls | grep flask-helm)
 if [ $? -ne "0" ]; then
    helm install flask-helm "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --version $TAG
