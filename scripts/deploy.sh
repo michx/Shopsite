eb167f1d2169c937d67413b6bb673d009681b7aa
Michele <micheleddd@gmail.com>
2023-04-17 16:35:20 +0200

    helm v20

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@ TAG=$1
 AWS_ECR_ACCOUNT_URL=$2
 AWS_ECR_HELM_REPO_NAME=$3
 echo "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME"
-
+export KUBECONFIG=/home/circleci/.kube/config
 result=$(eval helm ls | grep flask-helm)
 if [ $? -ne "0" ]; then
-   helm install flask-helm "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --version $TAG
+   helm install flask-helm "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --version $TAG -v 20
 else
-   helm upgrade flask-helm "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --version $TAG
+   helm upgrade flask-helm "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --version $TAG -v 20
 fi
