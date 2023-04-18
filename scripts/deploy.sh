7fd801a83f05aa5cbdecf37edb5c5f295c161d39
Michele <micheleddd@gmail.com>
2023-04-18 14:03:27 +0200

    ShoppingSite

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ echo "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME"
 export KUBECONFIG=/home/circleci/.kube/config
 result=$(eval helm ls | grep flask-helm)
 if [ $? -ne "0" ]; then
-   helm install flask-helm "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --version $TAG -v 20
+   helm install ShoppingSite "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --version $TAG
 else
-   helm upgrade flask-helm "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --version $TAG -v 20
+   helm upgrade ShoppingSite "oci://$AWS_ECR_ACCOUNT_URL/$AWS_ECR_HELM_REPO_NAME" --version $TAG
 fi
